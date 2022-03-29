f9b18cab6c38f727c4d92ced6b551dc685dcd70f
kingjalen22 <jalen.collins@uky.edu>
2022-03-29 03:16:41 -0400

    random photo storyboard and controller

--- Ranndom Photo/ViewController.swift
+++ Ranndom Photo/ViewController.swift
@@ -8,10 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .white
+        return imageView
+        
+    }()
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle("Random Photo", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+        
+    }()
+    let colors: [UIColor] = [
+        .systemPurple,
+        .systemRed,
+        .systemBlue,
+        .systemGray,
+        .systemPink,
+        .systemOrange]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPurple
+        view.addSubview(imageView)
+        imageView.frame = CGRect(x: 75, y: 300, width: 300, height: 300)
+        imageView.center = view.center
+        getRandomPhoto()
+       
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        view.addSubview(button)
+        button.frame = CGRect(x: 20, y: view.frame.size.height-50-view.safeAreaInsets.bottom, width: view.frame.size.width, height: 55)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+    }
+    @objc func didTapButton(){
+        getRandomPhoto()
+        view.backgroundColor = colors.randomElement()
+    }
+    func getRandomPhoto(){
+        let urlString = "https://source.unsplash.com/random/600x600"
+        let url = URL(string:urlString)!
+        guard let data = try? Data(contentsOf: url) else{
+            return
+        }
+        imageView.image = UIImage(data: data)
     }
 
 
